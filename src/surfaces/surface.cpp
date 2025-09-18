13819c473fe8949fb85dae61728be87252e54f9a
wkjarosz <wkjarosz@users.noreply.github.com>
2025-09-18 12:03:07 +0000

    deploy: cs87-dartmouth/darts-solutions@6ff8564d5624af5629ede51d46ca5d721d6c20b3

---
+++ src/surfaces/surface.cpp
@@ -0,0 +1,29 @@
+/*
+    This file is part of darts – the Dartmouth Academic Ray Tracing Skeleton.
+
+    Copyright (c) 2017-2025 by Wojciech Jarosz
+*/
+
+#include <darts/scene.h>
+#include <darts/surface.h>
+
+XformedSurface::XformedSurface(const json &j) { m_xform = j.value("transform", m_xform); }
+
+
+
+XformedSurfaceWithMaterial::XformedSurfaceWithMaterial(const json &j) : XformedSurface(j)
+{
+    m_material = DartsFactory<Material>::find(j);
+}
+
+
+
+/**
+    \file
+    \brief Surface, XformedSurface, and XformedSurfaceWithMaterial
+*/
+
+/**
+    \dir
+    \brief Darts Surface plugins source directory
+*/
